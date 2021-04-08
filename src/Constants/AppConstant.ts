b020c13179cc434d429014a72370932142e1bc85
duytung95nb <duytung95nb@gmail.com>
2021-04-08 15:14:30 +0700

    Integrate backend adapter

--- src/Constants/AppConstant.ts
+++ src/Constants/AppConstant.ts
@@ -1,4 +1,5 @@
 const APP_CONSTANT = {
   META_WEATHER_API: 'https://www.metaweather.com/api',
+  BACKEND_API: 'http://localhost:3001',
 };
 export default APP_CONSTANT;
--- src/DataServices/AxiosAdapter.ts
+++ src/DataServices/AxiosAdapter.ts
@@ -1,9 +1,10 @@
 // First we need to import axios.js
 import axios, { AxiosError } from 'axios';
+import APP_CONSTANT from '../Constants/AppConstant';
 // Next we make an 'instance' of it
-const AxiosAdapter = axios.create({});
+const AxiosNormal = axios.create({});
 
-AxiosAdapter.interceptors.response.use(
+AxiosNormal.interceptors.response.use(
   (result) => {
     return result.data;
   },
@@ -12,4 +13,28 @@ AxiosAdapter.interceptors.response.use(
   },
 );
 
+const AxiosBackEndAdapter = axios.create({});
+AxiosBackEndAdapter.interceptors.request.use(function (config) {
+  config.method = 'POST';
+  const requestedUrl = config.url;
+  config.url = APP_CONSTANT.BACKEND_API;
+  config.headers = {
+    'Content-Type': 'application/json',
+  };
+  config.data = {
+    url: requestedUrl,
+  };
+  return config;
+});
+
+AxiosBackEndAdapter.interceptors.response.use(
+  (result) => {
+    return result.data;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  },
+);
+const AxiosAdapter =
+  process.env.REACT_APP_USE_BACK_END_ADAPTER === 'true' ? AxiosBackEndAdapter : AxiosNormal;
 export default AxiosAdapter;

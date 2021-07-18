d8a8217a2825d44c88ea21d838e37a0147ca632b
Tung Dao <duytung95nb@gmail.com>
2021-07-19 00:04:05 +0700

    Apply adapter for Meta weather api

--- src/DataServices/GeolocationDataService.ts
+++ src/DataServices/GeolocationDataService.ts
@@ -2,19 +2,19 @@ import { DefaultLocation as DefaultLocation } from '../Models/DefaultLocation';
 import APP_CONSTANT from '../Constants/AppConstant';
 import { Location } from '../Containers/Search/Models';
 import AxiosOriginal from './AxiosOriginal';
+import AxiosAdapter from './AxiosAdapter';
 class GeolocationDataService {
   getByIpAddress() {
     return AxiosOriginal.get<DefaultLocation>('http://www.geoplugin.net/json.gp');
   }
   searchByText(text: string) {
-    return AxiosOriginal.get<Location[]>(
+    return AxiosAdapter.get<Location[]>(
       `${APP_CONSTANT.META_WEATHER_API}/location/search/?query=${text}`,
     );
   }
   searchByLatLong(lat: number, long: number) {
     // Example 36.96,-122.02
-    return AxiosOriginal.get<Location>(
-      // eslint-disable-next-line max-len
+    return AxiosAdapter.get<Location>(
       `${APP_CONSTANT.META_WEATHER_API}/location/search/?lattlong=${lat},${long}`,
     );
   }

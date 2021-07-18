51c2ec423e523406f503116a485f71b7debe58a7
Tung Dao <duytung95nb@gmail.com>
2021-07-18 22:47:37 +0700

    Use original axios for Geo location service

--- src/DataServices/GeolocationDataService.ts
+++ src/DataServices/GeolocationDataService.ts
@@ -1,6 +1,5 @@
 import { DefaultLocation as DefaultLocation } from '../Models/DefaultLocation';
 import APP_CONSTANT from '../Constants/AppConstant';
-import AxiosAdapter from './AxiosAdapter';
 import { Location } from '../Containers/Search/Models';
 import AxiosOriginal from './AxiosOriginal';
 class GeolocationDataService {
@@ -8,13 +7,13 @@ class GeolocationDataService {
     return AxiosOriginal.get<DefaultLocation>('http://www.geoplugin.net/json.gp');
   }
   searchByText(text: string) {
-    return AxiosAdapter.get<Location[]>(
+    return AxiosOriginal.get<Location[]>(
       `${APP_CONSTANT.META_WEATHER_API}/location/search/?query=${text}`,
     );
   }
   searchByLatLong(lat: number, long: number) {
     // Example 36.96,-122.02
-    return AxiosAdapter.get<Location>(
+    return AxiosOriginal.get<Location>(
       // eslint-disable-next-line max-len
       `${APP_CONSTANT.META_WEATHER_API}/location/search/?lattlong=${lat},${long}`,
     );
